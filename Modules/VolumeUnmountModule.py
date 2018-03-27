c2ffca6eeb57c3ab08b1559f6cd3bbc1bdb1c7e1
Corey <corey.wanless@wwt.com>
2018-03-26 21:37:25 -0500

    Added Unmount Module

---
+++ Modules/VolumeUnmountModule.py
@@ -0,0 +1,102 @@
+#============================================================
+#
+#
+# Copyright (c) 2017 NetApp, Inc. All rights reserved.
+# Specifications subject to change without notice.
+#
+# This sample code is provided AS IS, with no support or
+# warranties of any kind, including but not limited to
+# warranties of merchantability or fitness of any kind,
+# expressed or implied.
+#
+# Min Python Version = python 2.7
+#
+#============================================================
+
+
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+
+import requests
+import warnings
+import sys
+import json
+import time
+warnings.filterwarnings("ignore")
+
+def post():
+    url_path        = "/api/2.0/ontap/"
+    url_path+="volumes/"+key+"/jobs/unmount"
+
+    payload={}
+    if (force != None):
+        payload['force']=force
+    response=http_request_for_post(url_path,**payload)
+    json_response=response.headers
+    return json_response
+
+def http_request_for_get(url_path,**payload):
+	response = requests.get("https://"+api_host+":"+api_port+url_path, auth=(api_user_name,api_user_password), verify=False, data=json.dumps(payload),headers={'content-type': 'application/json'})
+	return response
+
+def http_request_for_put(url_path,**payload):
+	response = requests.put("https://"+api_host+":"+api_port+url_path, auth=(api_user_name,api_user_password), verify=False, data=json.dumps(payload),headers={'content-type': 'application/json'})
+	return response
+
+def http_request_for_post(url_path,**payload):
+	response = requests.post("https://"+api_host+":"+api_port+url_path, auth=(api_user_name,api_user_password), verify=False, data=json.dumps(payload),headers={'content-type': 'application/json'})
+	return response
+
+def http_request_for_delete(url_path,**payload):
+	response = requests.delete("https://"+api_host+":"+api_port+url_path, auth=(api_user_name,api_user_password), verify=False, data=json.dumps(payload),headers={'content-type': 'application/json'})
+	return response
+
+
+
+def main():
+        fields = {
+                "action" : {
+                        "required": True,
+                        "choices": ['post'],
+                        "type": 'str'
+                        },
+                "host" : {"required": True, "type": "str"},
+                "port" : {"required": True, "type": "str"},
+                "user" : {"required": True, "type": "str"},
+                "password" : {"required": True, "type": "str"},
+                "key" : {"required": False, "type": "str"},
+                "force" : {"required": False, "type": "str"},
+                }
+
+        module = AnsibleModule(argument_spec=fields)
+
+        # NetApp Service Level Manager details
+        global api_host
+        global api_port
+        global api_user_name
+        global api_user_password
+
+        global lun_key
+        global nfs_share_key
+        global cifs_share_key
+        api_host                = module.params["host"]
+        api_port                = module.params["port"]
+        api_user_name           = module.params["user"]
+        api_user_password       = module.params["password"]
+
+        # Properties details
+        global key
+        key = module.params["key"]
+        global force
+        force = module.params["force"]
+
+        global json_response
+
+        # Actions
+        if module.params["action"] == "post":
+                result=post()
+                module.exit_json(changed=True,meta=result['Location'].split("/jobs/")[1])
+
+if __name__ == '__main__':
+    main()
